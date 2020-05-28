4655c5f58a5b88ce91feff58d19d5916c2706218
PvlChe <Cafebryttoo95>
2020-05-28 18:10:55 +0200

    warnings. Prototype_01

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -11,28 +11,47 @@ exports.postQuiz = (req, res, next) => {
     const code = req.body.code;
     const complexities = req.body.complexities;
     const categories = req.body.categories;
+    let canceled = false;
     let tempQuiz;
+    let tempQuizcode;
     sequelize.transaction(createQuiz => {
-        return Quiz.create({
-            playerAge: playerAge,
-            playerPlz: playerPlz
+        return Quizcode.findOne({
+            where: {code: code}
         }, {transaction: createQuiz})
-        .then(quiz => {
-            tempQuiz = quiz;
-            return Quizcode.findOne({
-                where: {code: code}
-            }, {transaction: createQuiz});
+        .then(quizcode => {
+            if (quizcode.played) {
+                // redirect if quizcode was already played
+/*                Quiz.findOne({
+                    where: {quizcodeId: quizcode.id}
+                }).then( quiz => {
+                    res.status(200).json({played: true, quizId: quiz.id});
+                });*/
+            res.status(202).json({played: true});
+            canceled = true;
+            } else {
+                quizcode.played = true;
+                return quizcode.save({transaction: createQuiz});
+            }
         })
         .then(quizcode => {
-            quizcode.played = true;
-            return quizcode.save({transaction: createQuiz});
-        })
-        .then(result => {
-            tempQuiz.setQuizcode(result);
-            tempQuiz.userId = result.userId;
-            return tempQuiz.save({transaction: createQuiz});
+            if (canceled) return;
+            tempQuizcode = quizcode;
+            return Quiz.create({
+                playerAge: playerAge,
+                playerPlz: playerPlz
+            }, {transaction: createQuiz})
         })
         .then(quiz => {
+            if (canceled) return;
+
+            tempQuiz = quiz;
+            tempQuiz.setQuizcode(tempQuizcode);
+            tempQuiz.userId = tempQuizcode.userId;
+            return tempQuiz.save({transaction: createQuiz});
+        })
+        .then(result => {
+            if (canceled) return;
+
             return Question.findAll({
                 where: {
                     complexity: complexities,
@@ -41,6 +60,8 @@ exports.postQuiz = (req, res, next) => {
             }, {transaction: createQuiz});
         })
         .then( questions => {
+            if (canceled) return;
+
             const questionList = [];
             for (let i = 0; i < 5; i++) {
                 questionList.push(questions.splice(Math.random()*(questions.length - 1), 1)[0].dataValues);
@@ -50,7 +71,8 @@ exports.postQuiz = (req, res, next) => {
         })
     }).catch(err => {
         console.log(err);
-        res.end();
+        res.status(404);
+        res.send(err);
         next();
     })
 };
