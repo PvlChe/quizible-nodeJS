10ac5783e07fa629952db2104c208a40d63518c1
Pavel Chachotkin <pavel.chachotkin@partner.bmw.de>
2020-11-20 14:34:58 +0100

    Add new landing page

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 // bodyparser
